2a991e6dd4a2ec4da2d000227cb9801d4f687bd3
Nicole Lewis <lewis.nicole@gmail.com>
2012-12-09 17:26:57 -0800

    Adding .travis.yml

---
+++ week9/exercises/test.rb
@@ -0,0 +1,12 @@
+# encoding: UTF-8
+# This is a thing
+# We are learning about rdoc
+#Author:: Nicole
+class Thing
+	#This is an rdoc thing
+	# * The Method something just yields
+	# * Nothing else to see here :)
+	def something
+		yield
+	end
+end
